099f4f5d05c3e99d662c12b7f5704347549ff2cf
Hugh <deng.ch@northeastern.edu>
2019-04-23 02:21:20 -0700

    test one to many

--- LoadBalancer/src/main/java/com/obiew/Entities/Like.java
+++ LoadBalancer/src/main/java/com/obiew/Entities/Like.java
@@ -1,21 +1,19 @@
 package com.obiew.Entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity(name = "Like")
+@Table(name = "like")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long likeId;
+    @ManyToOne
+    @JoinColumn(name = "userId")
     private User user;
 
-    public Like(long likeId, User user) {
-        this.likeId = likeId;
-        this.user = user;
-    }
+    public Like() {}
 
     public long getLikeId() {
         return likeId;
@@ -25,33 +23,7 @@ public class Like {
         this.likeId = likeId;
     }
 
-    public User getUser() {
-        return user;
-    }
-
     public void setUser(User user) {
         this.user = user;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Like like = (Like) o;
-        return getLikeId() == like.getLikeId() &&
-                Objects.equals(getUser(), like.getUser());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getLikeId(), getUser());
-    }
-
-    @Override
-    public String toString() {
-        return "Like{" +
-                "likeId=" + likeId +
-                ", user=" + user +
-                '}';
     }
 }

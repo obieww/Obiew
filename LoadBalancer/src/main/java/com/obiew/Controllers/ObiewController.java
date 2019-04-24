35b8ac8f43c6656503b4ae83ff12f40f2c78963d
Hugh <deng.ch@northeastern.edu>
2019-04-24 15:52:24 -0700

    temp commit

--- LoadBalancer/src/main/java/com/obiew/Controllers/ObiewController.java
+++ LoadBalancer/src/main/java/com/obiew/Controllers/ObiewController.java
@@ -1,11 +1,21 @@
 package com.obiew.Controllers;
 
+import com.obiew.Entities.Obiew;
+import com.obiew.Repositories.ObiewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/obiew")
 public class ObiewController {
-
+    @Autowired
+    private ObiewRepository obiewRepository;
+    @GetMapping("/{obiewId}")
+    public ResponseEntity<Obiew> getObiewById(@PathVariable String obiewId) {
+        Obiew obiew =
+    }
 }
